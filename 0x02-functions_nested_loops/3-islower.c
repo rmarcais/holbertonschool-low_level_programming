e19159f7d9ad68a02848d03800509f799c48be9f
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-05 14:23:02 +0200

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
