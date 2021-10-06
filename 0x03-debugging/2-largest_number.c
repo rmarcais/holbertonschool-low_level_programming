373f17b88c783c8841aa7279adc9569adaf4faad
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-06 14:39:28 +0200

    0>972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,21 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a != b && a != c && b != c)
 	{
-		largest = a;
-	}
-	if (b > a && b > c)
-	{
-		largest = b;
-	}
-	if (c > b && c > a)
-	{
-		largest = c;
-	}
+		if (a > b && a > c)
+		{
+			largest = a;
+		}
+		else if (b > a && b > c)
+		{
+			largest = b;
+		}
+		else
+		{
+			largest = c;
+		}
 
-	return (largest);
+		return (largest);
+	}
 }
