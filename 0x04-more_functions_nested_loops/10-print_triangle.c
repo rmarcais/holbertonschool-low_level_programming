188385542c3e503307fdc0f275c38da1954c8de2
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-07 14:38:07 +0200

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - check the code
+ *@size: take an integer value
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int a, b, c, s1 = size - 1;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for(b=  0; b < s1; b++)
+			{
+				_putchar(' ');
+			}
+			for (c = s1; c < size; c++)
+			{
+				_putchar('#');
+			}
+			s1--;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
