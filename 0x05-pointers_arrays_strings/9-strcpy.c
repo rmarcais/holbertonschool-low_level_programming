686456892a160c6ca359706909f7ebcedadb5fc3
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-11 16:30:04 +0200

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - check the code
+ *@dest: take a character value
+ *@src: take an other character value
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+	return (src);
+}
