3720a2d4bf3a349b5c606a44090f8a414c6484d0
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-11 11:41:04 +0200

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - check the code
+ *@str: take a character value
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i, len = strlen(str);
+
+	for (i = 0; i <= len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
