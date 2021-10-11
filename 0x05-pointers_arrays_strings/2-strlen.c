56c7e09950a8f6167041d30e6f92f00624aa8eb4
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-11 11:05:47 +0200

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - check the code
+ *@s: take a character value
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len1;
+
+	len1 = strlen(s);
+	return (len1);
+}
