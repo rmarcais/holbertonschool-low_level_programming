11d407146d1bb547cb517aa89ba451ebd35f453c
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-13 12:54:40 +0200

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenates two strings
+ *@dest: points to a variable char
+ *@src: points to a variable char
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
