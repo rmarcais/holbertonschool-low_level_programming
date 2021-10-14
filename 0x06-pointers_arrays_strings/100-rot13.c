a52bd324e6e925c46d664b2d429b11cd6a666f79
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-14 10:34:28 +0200

    encode using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rot13 - encode a string using rot13
+ *@s: points to a variable char
+ * Return: Always 0.
+ */
+char *rot13(char *str)
+{
+	int i, id, len = strlen(str);
+	char s1[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char s2[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	for (i = 0; i < len; i++)
+	{
+		for (id = 0; id < 52; id++)
+		{
+			if (str[i] == s1[id])
+			{
+				str[i] = s2[id];
+				break;
+			}
+		}
+	}
+	return (str);
+}
