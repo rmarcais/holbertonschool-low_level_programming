be39742f2b62959f2f0cdf819fbce0ee0cb19eda
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-18 18:34:33 +0200

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,19 @@
 #include "main.h"
 #include <string.h>
 /**
- * _memcpy - prints buffer in hexa
+ * _memcpy - copies memory area
  * @dest: points to a character
  * @src: points to a character
  *@n: takes an unigned integer value
- * Return: Nothing.
+ * Return: dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	return (memcpy(dest, src, n));
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
