19c6e70efdde43afc013f11a52294800984bc691
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-18 12:46:33 +0200

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strpbrk - check the code
+ *@s: points to a character
+ *@accept: points to a character
+ * Return: a pointer.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
