1015687f02461c52e3e66a05c6ea4a550719af8d
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-18 23:49:36 +0200

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,5 +8,27 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	return (strstr(haystack, needle));
+	int i = 0, j = 0;
+
+	while (*(haystack + i) != '\0')
+	{
+		if (*(haystack + i) == *(needle + j))
+		{
+			j++;
+			if(*(needle + j) - (j - 1))
+			{
+				return ((haystack + i) - (j - 1));
+			}
+		}
+		else
+		{
+			j = 0;
+		}
+	        i++;
+	}
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	return ('\0');
 }
