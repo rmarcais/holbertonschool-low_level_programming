909a7b819cc090ca6aa32210fc2f2ae0ea15c070
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-18 10:12:30 +0200

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - locates a character string
+ *@s: points to a character
+ *@c: character
+ * Return: pointer.
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
