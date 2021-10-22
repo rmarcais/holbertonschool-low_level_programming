f255f7dc62db43a5783f88c09588efd81d3f4250
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-22 10:04:36 +0200

    fix errors Betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 /**
  * main - prints the name of the proram
- *@agrv: array of pointers
+ *@argv: array of pointers
+ *@argc: number of arguments
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
