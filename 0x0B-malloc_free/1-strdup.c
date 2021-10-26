a424b327f83cffa20acb803f5142e743ea7d0a0e
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-26 11:08:18 +0200

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: points to a char
+ *
+ * Return: str or NULL
+ */
+int _strlen(char *str);
+char *_strdup(char *str)
+{
+	int len = _strlen(str);
+	char *na = malloc(sizeof(char) * len);
+	int id = 0;
+
+	if (str[len - 1] == '\0')
+		return ('\0');
+	while (str[id])
+	{
+		na[id] = str[id];
+		id++;
+	}
+	return (na);
+}
+/**
+ * _strlen - computes the size of str
+ * @str: points to a char
+ * Return: count.
+ */
+int _strlen(char *str)
+{
+	int i = 0, count;
+	while(str[i])
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
