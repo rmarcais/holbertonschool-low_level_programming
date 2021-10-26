a118a496416836ab18a593bdd075f795a3a636ed
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-26 18:44:38 +0200

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free _grid - frees a 2 dimensional grid
+ * free_grid - frees a 2 dimensional grid
  * @grid: grid to free
  *@height: height of the grid
  * Return: Nothing.
