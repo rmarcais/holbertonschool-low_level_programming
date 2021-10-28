1351321c6c37c758de990f061a398ce7b233dbd8
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-28 13:03:05 +0200

    fix errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 	len1 = _strlen(s1);
-	ar = malloc((len1 + n) * sizeof(char) + 1);
+	ar = malloc((len1 + n) * sizeof(char));
 
 	if (ar == 0)
 		return (NULL);
