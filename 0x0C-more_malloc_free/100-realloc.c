7d0fc177dd5160780d1d5abe84a15e9d0fb04c02
Rémi Marçais <remi.marcais87@gmail.com>
2021-10-29 10:44:09 +0200

    fix errors

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		return(malloc(new_size));
+		return (malloc(new_size));
 	}
 	if (new_size == 0 && ptr != 0)
 	{
