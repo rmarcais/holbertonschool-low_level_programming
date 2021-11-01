5233a760882c63c491decf627bf81748fe97bde3
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-01 19:54:59 +0100

    free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dogs
+ * @d: points to a variable of type dog_t
+ * Return: Nothing.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
