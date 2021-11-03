d647175fa7f665cbbf89f1242435a42826e6047b
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-03 13:05:00 +0100

    Fix errors

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,20 +10,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if(array != 0 && size != 0 && cmp != 0)
-	{
-		if (size <= 0)
-			return (-1);
-		while (i < size)
-		{
-			if (cmp(array[i]) == 1)
-			{
-				cmp(array[i]);
-				return (i);
-			}
-			i++;
+	if (cmp == 0)
+		return (0);
+
+	if (size <= 0)
+       		return (-1);
+	while (i < size)
+       	{
+		if (cmp(array[i]) == 1)
+	       	{
+       			cmp(array[i]);
+			return (i);
 		}
-		return (-1);
+	       	i++;
 	}
-	return (0);
+       	return (-1);
 }
