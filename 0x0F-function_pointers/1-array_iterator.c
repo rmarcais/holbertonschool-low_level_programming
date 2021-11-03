dc476c93d069bca04ba543e46dcb6771a1c6e77c
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-03 12:53:29 +0100

    Fix errors

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array != 0 || size != 0 || action != 0)
+	if (array != 0 && size != 0 && action != 0)
 	{
 		for (i = 0; i < size; i++)
 		{
