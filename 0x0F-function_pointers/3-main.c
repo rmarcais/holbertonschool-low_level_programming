bbc5771d6fad0f0f0d2a1af9ce92ab162d2b6851
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-03 16:57:20 +0100

    Fix errors

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,6 +15,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	fp = get_op_func(argv[2]);
