979762f26cea8a426c8da08665d81792e25f2b20
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-03 10:31:35 +0100

    Fix errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,10 @@
 #include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: points to the name
+ * @f: points to a function
+ * Return: Nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != 0)
