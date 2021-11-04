96383deab2f680987ba38a60bc443f84d281cc37
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-04 23:41:28 +0100

    assembly

---
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -0,0 +1,17 @@
+section .data
+
+	message db "Hello, world", 10
+
+section .text
+
+global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 13
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
