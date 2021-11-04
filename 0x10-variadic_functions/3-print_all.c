5c383c189e3bfc706cd7fe4284990159d5207271
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-04 18:03:57 +0100

    print_all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,87 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ * Return: Nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int i = 0, j = 0;
+	type_t remi[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"s", print_str},
+		{"f", print_float},
+		{NULL, NULL}
+		};
+	char *separator = "";
+
+	va_start(ap, format);
+
+	while (format[i])
+	{
+		j = 0;
+		while (remi[j].type)
+		{
+			if (format[i] == *remi[j].type)
+			{
+				remi[j].f(separator, ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
+/**
+ * print_char - prints a character
+ * @separator: separator
+ * @ap: name of the va_list
+ * Return: Nothing.
+ */
+void print_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+}
+/**
+ * print_int - prints an integer
+ * @separator: separator
+ * @ap: name of the va_list
+ * Return: Nothing.
+ */
+void print_int(char *separator, va_list ap)
+{
+	printf("%s%d", separator, va_arg(ap, int));
+}
+/**
+ * print_str - prints a string
+ * @separator: separator
+ * @ap: name of the va_list
+ * Return: Nothing.
+ */
+void print_str(char *separator, va_list ap)
+{
+	char *string;
+
+	string = va_arg(ap, char *);
+	if (string == 0)
+	{
+		printf("%s(nil)", separator);
+		return;
+	}
+	printf("%s%s", separator, string);
+}
+/**
+ * print_float - prints a float
+ * @separator: separator
+ * @ap: name of the va_list
+ * Return: Nothing.
+ */
+void print_float(char *separator, va_list ap)
+{
+	printf("%s%f", separator, va_arg(ap, double));
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,4 +5,20 @@ int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struct type - Struct op
+ *
+ * @type: type of the variable
+ * @f: points to fonctions
+ */
+typedef struct type
+{
+	char *type;
+	void (*f)(va_list ap);
+} type_t;
+void print_char(char *separator, va_list ap);
+void print_int(char *separator, va_list ap);
+void print_str(char *separator, va_list ap);
+void print_float(char *separator, va_list ap);
 #endif
