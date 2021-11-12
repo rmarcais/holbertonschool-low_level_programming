0eb24fbf875e8b23385695135ba4575416db09ca
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-12 16:23:39 +0100

    Add the function print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * print_list - prints all the element of a list_t list.
+ * @h: list to print
+ *
+ * Return: The number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == 0)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
+		count++;
+	}
+	return (count);
+}
