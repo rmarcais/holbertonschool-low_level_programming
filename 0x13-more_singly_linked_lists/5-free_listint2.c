cff50fda9e2c70257b740160d785ec6ef4f60a19
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-16 12:02:28 +0100

    Fix errors

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if(head == NULL || *head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		tmp = *head;
 		*head = (*head)->next;
 		free(tmp);
 	}
-	*head = NULL;
 	free(*head);
+	*head = NULL;
 }
