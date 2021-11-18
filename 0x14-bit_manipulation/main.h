d02a457f7882ecf2ff10816112d0f2dd124a2944
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-18 11:28:00 +0100

    Add all new files

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+int _putchar(char c);
+int _strlen(const char *str);
+unsigned int binary_to_uint(const char *b);
+long int _atoi(const char *s);
+#endif
