0916ea313488afb39a7acf5e8b3f194a92dcbd29
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-18 11:58:49 +0100

    Improve code. Fix errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,25 +7,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, a, rem, len, sum, base;
+	unsigned int decimal_num, base;
+	int len;
 
 	if (b == NULL)
 		return (0);
 	len = _strlen(b);
-	sum = 0;
 	base = 1;
-	for (i = 0; i < len; i++)
-		if (b[i] != '1' && b[i] != '0')
-		return (0);
-	a = _atoi(b);
-	while (a > 0)
+	decimal_num = 0;
+	for (len = len - 1; len >= 0; len--)
 	{
-		rem = a % 10;
-		sum += rem * base;
-		a /= 10;
+		if (b[len] != '1' && b[len] != '0')
+			return (0);
+		decimal_num += (b[len] - '0') * base;
 		base *= 2;
 	}
-	return (sum);
+	return (decimal_num);
 }
 /**
  * _strlen - computes the size of str
@@ -42,32 +39,4 @@ int _strlen(const char *str)
 		i++;
 	}
 	return (count);
-}
-/**
- * _atoi - check the code
- *@s: taje a character value
- * Return: Always 0.
- */
-long int _atoi(const char *s)
-{
-	int sign = 1;
-	long int num = 0;
-
-	while (*s != '\0')
-	{
-		if (*s == '-')
-		{
-			sign *= -1;
-		}
-		else if (*s >= 48 && *s <= 57)
-		{
-			num = num * 10 + (*s - '0');
-		}
-		else if (num > 0)
-		{
-			break;
-		}
-		s++;
-	}
-	return (num * sign);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,5 +4,4 @@
 int _putchar(char c);
 int _strlen(const char *str);
 unsigned int binary_to_uint(const char *b);
-long int _atoi(const char *s);
 #endif
