0ee9a82e93073860230d5bbe6fc63ffde8539dc8
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-19 13:16:18 +0100

    Add the 100-get_endianness.c file

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness.
+ * Return: 1 or 0.
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
