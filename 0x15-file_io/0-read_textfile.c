3118a442ed95bcfaee65d02c58aabe715cfb6dac
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-22 13:09:24 +0100

    Add all new files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX stdout.
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ * Return: 0 or the actual number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, r;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	r = read(fd, buf, letters);
+	write(1, buf, r);
+	free(buf);
+	close(fd);
+	return (r);
+}
