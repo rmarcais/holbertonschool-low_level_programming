522ec9852462de3924ff021926d062212f791e5f
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-22 14:36:37 +0100

    Add the 1-create_file.c file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: The name of the file
+ * @text_content: content of the text
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+	char *buf;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		buf = "";
+	else
+		buf = text_content;
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	w = write(fd, buf, _strlen(text_content));
+	if (fd == -1 || w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
+/**
+ * _strlen - computes the size of str
+ * @str: points to a char
+ * Return: count.
+ */
+int _strlen(char *str)
+{
+	int i = 0, count = 0;
+
+	while (str[i])
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
