f948a93dfa85d47d8c342526faefb975559b0bb6
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-06 15:21:57 +0100

    Fix bugs

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,7 +20,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(tmp);
 		return (1);
 	}
-	for (i = 0; i < index - 1; i++)
+	for (i = 0; i < index - 2; i++)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
