af4864537a83d80d0e7976e47c88d0300134d37c
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-28 00:52:13 +0100

    Fix bugs

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
