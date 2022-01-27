7d2ff8012219562286b9fcad7443e651fea2c9bf
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-28 00:23:17 +0100

    Fix bugs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new, *tmp;
 
-	if (strcmp(key, "") == 0 || strcmp(value, "") == 0)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
