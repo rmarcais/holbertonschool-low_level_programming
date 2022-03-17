8b08a1aa71173ea279cf2412022c9e3d26b294d9
Rémi Marçais <remi.marcais87@gmail.com>
2022-03-17 13:44:55 +0100

    Fix bugs

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,11 +4,11 @@
 
 def island_perimeter(grid):
     """Function that return the perimeter of the island described in 'grid'."""
-    if grid is None or grid == [[]]:
-        return
     perimeter = 0
     for i in range(1, len(grid) - 1):
         for j in range(1, len(grid[i]) - 1):
+            if grid[i][j] > 1:
+                return
             if grid[i][j] == 1:
                 if grid[i - 1][j] == 0:
                     perimeter += 1
