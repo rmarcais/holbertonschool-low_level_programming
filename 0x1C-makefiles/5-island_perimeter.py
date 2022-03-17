5b108ab10c62a10005da4029ee654c933fd715a1
Rémi Marçais <remi.marcais87@gmail.com>
2022-03-17 12:00:21 +0100

    Add all files

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines the function island_perimeter"""
+
+
+def island_perimeter(grid):
+    """Function that return the perimeter of the island described in 'grid'."""
+    perimeter = 0
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid[i]) - 1):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
