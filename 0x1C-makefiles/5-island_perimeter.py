8c048f8fe503e82a01720ddee48e84aacf005290
Rémi Marçais <remi.marcais87@gmail.com>
2022-03-17 14:24:13 +0100

    improve code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,7 +10,7 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
+                if j == 0 grid[i][j - 1] == 0:
                     perimeter += 1
                 if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
