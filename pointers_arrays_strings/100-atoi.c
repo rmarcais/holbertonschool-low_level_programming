db0f6cd8f0fa740628ccf3edd294b370006d4825
rmarcais <remi.marcais87@gmail.com>
2024-11-05 11:01:19 +0100

    Test

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _atoi - check the code
+ *@s: taje a character value
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if (*s >= 48 && *s <= 57)
+		{
+			num = num * 10 + (*s - '0');
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+		s++;
+	}
+	return (num * sign);
+}
