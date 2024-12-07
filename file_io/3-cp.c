74957a0022a5a3e4741654b4ae1bade71597a82a
Rémi Marçais <remi.marcais87@gmail.com>
2024-12-07 14:25:01 +0100

    Try to fix bug

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -36,12 +36,6 @@ int main(int ac, char *av[])
 		exit(98);
 	}
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		handle_close(fd_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
 	while ((read_bytes = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		write_bytes = write(fd_to, buffer, read_bytes);
